fb1b3d5d4a4d1135f8e54c478be5ea295b24df78
madildevops <111339373+madildevops@users.noreply.github.com>
2022-10-04 16:19:06 +0500

    First commit to repo2

---
+++ sample.py
@@ -0,0 +1,8 @@
+# Assign a numeric value
+number = 70
+
+# Check the is more than 70 or not
+if (number >= 70):
+    print("Marks are greater than seventy")
+else:
+    print("Marks are below the limit")
